d3c23b80c0879a77ab9298e015d0e56183862821
Emerald <emerald.zhang@viam.com>
2024-07-02 12:43:39 -0400

    github actions

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+UNAME=$(uname -s)
+if [ "$UNAME" = "Linux" ]
+then
+    if dpkg -l python3-venv; then
+    echo "python3-venv is installed, skipping setup"
+    else
+    echo "Installing venv on Linux"
+    sudo apt-get install -y python3-venv
+    fi
+fi
+if [ "$UNAME" = "Darwin" ]
+then
+echo "Installing venv on Darwin"
+brew install python3-venv
+fi
+python3 -m venv .venv . 
+source .venv/bin/activate 
+pip3 install -r requirements.txt 
+python3 -m PyInstaller --onefile --hidden-import="googleapiclient" src/__main__.py 
+tar -czvf dist/archive.tar.gz dist/__main__
